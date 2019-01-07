c9ed6bff42294565315352494520978404683c4c
skarus <kovrizhin96@mail.ru>
2019-01-07 19:31:40 +0300

    ТРЕТЬЕ ЗАДАНИЕ ЕБА

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,21 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def create
-    Todo.create(todo_params)
-    redirect_to :root_url
+    @todo = Todo.new(params.require(:todo).permit([:text, :project_id]))
+		if @todo.save
+			redirect_to projects_path
+		end
   end
 
-  private
-  def todo_params
-    params.require(:todo).permit(:text, :project_id)
+  def update
+		@todo = Todo.find(params[:id])
+		@todo.update(:isCompleted => !@todo.isCompleted)
   end
+  
+  # private
+  # def todo_params
+  #   params.require(:todo).permit(:text, :project_id)
+  # end
+
+  
 end
--- db/migrate/20181221211343_create_todos.rb
+++ db/migrate/20181221211343_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[5.1]
   def change
     create_table :todos do |t|
       t.string :text
-      t.boolean :isCompleted
+      t.boolean :isCompleted, default: false
       t.references :project, foreign_key: true
 
       t.timestamps
